32b963a9ab2f376a573e6c6e05552d71702ee25b
RiccardoFossati <fossati.riccardo2810@gmail.com>
2017-11-21 04:28:24 +0100

    Creazione File Main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+Riccardo Fossati 5AIA 21/11/2017
+COmpito TPSIT
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+     int iC=0;
+     
+     for (int i=0; i<10; i++)
+     {
+         iC++;
+     }
+         
+      cout << iC << endl;
+      system("PAUSE");
+      return 0;   
+}
+         
